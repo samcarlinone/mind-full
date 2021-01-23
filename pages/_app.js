c6fabe09ce36737311354bb7ff4766d63969aed2
Sam Carlin <samcarlinone@gmail.com>
2021-01-23 17:22:41 -0500

    Fix favicon names.

--- pages/_app.js
+++ pages/_app.js
@@ -14,13 +14,13 @@ const App = ({ Component, pageProps }) => (
       />
       <link rel="manifest" href="/manifest.json" />
       <link
-        href="/icons/favicon-16x16.png"
+        href="/icons/icon-16x16.png"
         rel="icon"
         type="image/png"
         sizes="16x16"
       />
       <link
-        href="/icons/favicon-32x32.png"
+        href="/icons/icon-32x32.png"
         rel="icon"
         type="image/png"
         sizes="32x32"
