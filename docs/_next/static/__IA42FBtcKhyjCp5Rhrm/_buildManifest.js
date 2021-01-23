ccb98ab5006022d0efaf466c99ad919b87ff1e81
Sam Carlin <samcarlinone@gmail.com>
2021-01-23 17:16:31 -0500

    Switching to vercel hosting.

--- docs/_next/static/__IA42FBtcKhyjCp5Rhrm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F0aac4da5d6d907d9a7db.css","static\u002Fchunks\u002Fpages\u002Findex-9c6c1a73070cf506eead.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
